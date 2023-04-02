2320df4660837563b40a8c0471d01aca97742b9c
Andukov Almaz <68683713+andiazdi@users.noreply.github.com>
2023-04-02 15:48:25 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+TOKEN = "TOKEN"
+admin_chat_id = [your_chat_id]
+mqtt_ip = '192.168.1.9'
+mqtt_port = 1883
+topic = "smart_home/mqtt"
+
+
+class bcolors:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
